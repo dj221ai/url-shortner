85ef13b9e7f08927dd82f823fad4570b88ca343b
Ved Jha <vedprakash@fanclash.in>
2024-03-07 07:47:49 +0530

    added shortner model

--- urlshortnerproject/shortner/models.py
+++ urlshortnerproject/shortner/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class ShortenedURL(models.Model):
+    original_url = models.URLField()
+    short_url = models.CharField(max_length=20, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.original_url} converted to {self.short_url}'
