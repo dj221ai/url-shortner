f65d9e9127007110876f4c90c03bba5706dd1709
Ved Jha <vedprakash@fanclash.in>
2024-03-19 22:12:00 +0530

    api side

--- urlshortnerproject/urlshortnerproject/urls.py
+++ urlshortnerproject/urlshortnerproject/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shortner.urls')),
     path('account/', include('account.urls')),
+    path('api/', include('apishortner.urls')),
 ]
